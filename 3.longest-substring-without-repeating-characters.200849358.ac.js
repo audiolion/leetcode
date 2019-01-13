f536e7b591c65168a0385fbe25b83e3ba55662a4
Ryan Castner <castner.rr@gmail.com>
2019-01-12 22:22:56 -0500

    3, add sliding window solution

--- 3.longest-substring-without-repeating-characters.200849358.ac.js
+++ 3.longest-substring-without-repeating-characters.200849358.ac.js
@@ -13,45 +13,45 @@
  *
  * Given a string, find the length of the longest substring without repeating
  * characters.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: "abcabcbb"
- * Output: 3 
- * Explanation: The answer is "abc", with the length of 3. 
- * 
- * 
- * 
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: "bbbbb"
  * Output: 1
  * Explanation: The answer is "b", with the length of 1.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: "pwwkew"
  * Output: 3
- * Explanation: The answer is "wke", with the length of 3. 
+ * Explanation: The answer is "wke", with the length of 3.
  * ⁠            Note that the answer must be a substring, "pwke" is a
  * subsequence and not a substring.
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  */
 /**
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = (s) => {
+const lengthOfLongestSubstring = s => {
   let longestSubstring = 0;
   let seenChars = new Set();
   for (let i = 0; i < s.length; i++) {
@@ -69,4 +69,20 @@ const lengthOfLongestSubstring = (s) => {
     seenChars.add(ch);
   }
   return seenChars.size > longestSubstring ? seenChars.size : longestSubstring;
+};
+
+// Sliding Window solution
+
+const lengthOfLongestSubstringSlidingWindow = s => {
+  let longestSubstring = 0;
+  let seenChars = [];
+  for (let i = 0; i < s.length; i++) {
+    let ch = s.charAt(i);
+    while (seenChars.includes(ch)) {
+      seenChars.shift();
+    }
+    seenChars.push(ch);
+    longestSubstring = Math.max(seenChars.length, longestSubstring);
+  }
+  return longestSubstring;
 };
